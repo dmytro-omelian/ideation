723f105e2ae8ff224bfb41d6b26d467ff90178e0
Dichik <omeluan.dima@gmail.com>
2023-12-26 17:27:26 +0200

    update image uploader

--- ideation/src/image-uploading-scroll/ImageUploader.tsx
+++ ideation/src/image-uploading-scroll/ImageUploader.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
-import "./image-uploader.css"; // Create a separate CSS file for styling
+import "./image-uploader.css"; // Ensure this CSS file exists and is styled as required
 
 const ImageUploader = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [images, setImages] = useState<string[]>([]);
 
-  const handleImageChange = (e: any) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader: any = new FileReader();
-      reader.onloadend = () => {
-        setSelectedImage(reader.result);
-      };
-      reader.readAsDataURL(file);
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files);
+      const newImagesPromises: Promise<string>[] = filesArray.map((file) => {
+        return new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            resolve(reader.result as string);
+          };
+          reader.readAsDataURL(file);
+        });
+      });
+
+      Promise.all(newImagesPromises).then((newImages) => {
+        setImages((prevImages) => [...prevImages, ...newImages]);
+      });
     }
+  };
+
+  const removeImage = (index: number) => {
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
 
   return (
@@ -23,12 +35,33 @@ const ImageUploader = () => {
         accept="image/*"
         onChange={handleImageChange}
         className="image-uploader-input"
+        multiple
+        style={{ display: "none" }}
+        id="file-input"
       />
-      {selectedImage && (
-        <div className="image-preview-container">
-          <img src={selectedImage} alt="Selected" className="image-preview" />
-        </div>
-      )}
+      <button
+        onClick={() => document.getElementById("file-input")?.click()}
+        className="image-uploader-button"
+      >
+        <span className="plus-icon">+</span>{" "}
+      </button>
+      <div className="image-preview-container">
+        {images.map((image, index) => (
+          <div key={index} className="image-container">
+            <img
+              src={image}
+              alt={`Selected ${index}`}
+              className="image-preview"
+            />
+            <button
+              onClick={() => removeImage(index)}
+              className="remove-image-button"
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
