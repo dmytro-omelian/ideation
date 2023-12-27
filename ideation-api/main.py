57a3fee8b36707b02e994e63a34c334a2a8d4f5c
Dichik <omeluan.dima@gmail.com>
2023-12-27 07:57:28 +0200

    Create endpoint to get image to process

--- ideation-api/main.py
+++ ideation-api/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile = File(...)):
+    with open(file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return FileResponse(file.filename)
