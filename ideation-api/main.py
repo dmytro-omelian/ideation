757d57dddc7ce93013baaa6e5f7a189afca0e8f4
Dichik <omeluan.dima@gmail.com>
2023-12-28 08:41:29 +0200

    Update backend (SAM working)

--- ideation-api/main.py
+++ ideation-api/main.py
@@ -2,8 +2,40 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
+import os
+import torch
+import cv2
+import supervision as sv
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+# Model Initialization
+def initialize_model():
+    HOME = os.getcwd()
+    CHECKPOINT_PATH = os.path.join(HOME, "weights", "sam_vit_h_4b8939.pth")
+    DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    MODEL_TYPE = "vit_h"
+
+    sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH).to(device=DEVICE)
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    print('Model Initialized Successfully')
+    return mask_generator
+
+# Service for Model Processing
+def process_image_with_model(image_path, mask_generator):
+    image_bgr = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+    sam_result = mask_generator.generate(image_rgb)
+    mask_annotator = sv.MaskAnnotator(color_lookup=sv.ColorLookup.INDEX)
+    detections = sv.Detections.from_sam(sam_result=sam_result)
+
+    annotated_image = mask_annotator.annotate(scene=image_bgr.copy(), detections=detections)
+    output_path = os.path.join(os.getcwd(), "data", "annotated_image.jpeg")
+    cv2.imwrite(output_path, annotated_image)
+    return output_path
 
 app = FastAPI()
+mask_generator = initialize_model()
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,4 +57,6 @@ async def say_hello(name: str):
 async def create_upload_file(file: UploadFile = File(...)):
     with open(file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    return FileResponse('./data/annotated_dog.jpeg')
+
+    segmented_image_path = process_image_with_model(file.filename, mask_generator)
+    return FileResponse(segmented_image_path)
