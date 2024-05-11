910d3124ba3cf9310b09e85be5d27a3073fd831a
Dichik <omeluan.dima@gmail.com>
2024-05-11 17:21:37 +0300

    add nest-service

---
+++ nest-service/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'newuser',
+      password: 'password',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
