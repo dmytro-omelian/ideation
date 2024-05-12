3012bdfcff87dbc52e09092d35ebec1fc4a6d2cc
Dichik <omeluan.dima@gmail.com>
2024-05-12 09:36:12 +0300

    add AWS S3 bucket storage logic

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.uploadFile(file);
+  }
+
+  @Get(':key')
+  async getImage(@Param('key') key: string, @Res() res: Response) {
+    try {
+      const s3Url = `https://${this.appService.AWS_S3_BUCKET}.s3.eu-north-1.amazonaws.com/${key}`;
+      console.log(s3Url);
+      const imageData = await this.appService.getImageFromS3(s3Url);
+
+      // Set the appropriate content type based on the file extension or MIME type
+      res.set({
+        'Content-Type': 'image/png', // Change the content type according to your image type
+        'Content-Length': imageData.length.toString(),
+      });
+      res.send(imageData);
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      res.status(500).send('Error fetching image from S3');
+    }
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req, Res } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import * as axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  AWS_S3_BUCKET = 'do-ideation-diploma';
+  s3 = new AWS.S3({
+    accessKeyId: 'AKIAQ3D7IBZA6TSH6RQO',
+    secretAccessKey: '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',
+  });
+
+  async uploadFile(file) {
+    console.log(file);
+    const { originalname } = file;
+
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET,
+      originalname,
+      file.mimetype,
+    );
+  }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: 'ap-south-1',
+      },
+    };
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getImageFromS3(url: string): Promise<Buffer> {
+    try {
+      const response = await axios.default.get(url, {
+        responseType: 'arraybuffer',
+      });
+      return Buffer.from(response.data, 'binary');
+    } catch (error) {
+      console.error('Error fetching image from S3:', error);
+      throw error;
+    }
   }
 }
